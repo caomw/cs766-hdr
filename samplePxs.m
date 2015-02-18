8b6a7b1f5217a375140618e88a719d1438c6bdfa
malichan <ke_ma@outlook.com>
2015-02-17 18:45:22 -0600

    fix sampler bugs and rearrange gui

--- samplePxs.m
+++ samplePxs.m
@@ -1,20 +1,20 @@
 function Z = samplePxs(imgs)
 %% uniform sampler
-%imgNum = size(imgs,3);
-%smpNumSqrt = round(sqrt(2*256/(imgNum - 1)));
-%smpNum = smpNumSqrt*smpNumSqrt;
-%height = size(imgs,1);
-%width = size(imgs,2);
-%k = 1;
-%Z = zeros(smpNum,imgNum);
-%for i=1:smpNumSqrt
+% imgNum = size(imgs,3);
+% smpNumSqrt = round(sqrt(2*256/(imgNum - 1)));
+% smpNum = smpNumSqrt*smpNumSqrt;
+% height = size(imgs,1);
+% width = size(imgs,2);
+% k = 1;
+% Z = zeros(smpNum,imgNum);
+% for i=1:smpNumSqrt
 %   for j=1:smpNumSqrt
 %       y = round(i*height/(smpNumSqrt+1));
 %       x = round(j*width/(smpNumSqrt+1));
 %       Z(k,:) = imgs(y,x,:);
 %       k = k+1;
-%   end
-%end
+%    end
+% end
 %% random sampler
 imgNum = size(imgs,3);
 smpNum = round(2*256/(imgNum - 1));
@@ -24,14 +24,14 @@ width = size(imgs,2);
 Z = zeros(smpNum,imgNum);
 k = 1;
 miss = 0;
-mnThreshold = 3;
-bevThreshold = 6;
-wevThreshold = 12;
+mnThreshold = 6;
+bevThreshold = 12;
+wevThreshold = 24;
 relaxRate = 1.5;
 while 1
     if miss >= 10
         mnThreshold = mnThreshold * relaxRate;
-        bevThreshold = bevThreshold * relaxRate;
+        bevThreshold = bevThreshold / relaxRate;
         wevThreshold = wevThreshold * relaxRate;
         miss = 0;
     end
@@ -39,7 +39,7 @@ while 1
     i = floor(rand() * (height - 2)) + 2;
     j = floor(rand() * (width - 2)) + 2;
     
-    pixels = squeeze(imgs(i,j,:));
+    pixels = double(squeeze(imgs(i,j,:)));
     if pixels(imgNum) < pixels(1)
         pixels = flip(pixels);
     end
@@ -47,7 +47,7 @@ while 1
     for l = imgNum:-1:2
         increments(l-1) = pixels(l) - pixels(l-1);
     end
-    neighbors = imgs(i-1:i+1,j-1:j+1,round(imgNum/2));
+    neighbors = double(imgs(i-1:i+1,j-1:j+1,round(imgNum/2)));
     
     % monotonicity check
     if nnz(increments < -mnThreshold) > 0
@@ -62,7 +62,7 @@ while 1
         continue;
     end
     % within-exposure variance check
-    if std(double(neighbors(:))) > wevThreshold
+    if std(neighbors(:)) > wevThreshold
         %display(std(double(neighbors(:))));
         miss = miss + 1;
         continue;
